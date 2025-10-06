668664572e207568b783a25550c5fbfe947176ba
gabok2003 <gabriel.fonseca.guerrero@gmail.com>
2025-10-06 00:46:50 -0500

    Vista de una publicación y vista del perfil

--- frontend/MusicShareFrontend/src/App.tsx
+++ frontend/MusicShareFrontend/src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import SignUp from "./components/SignUp"
 import Login from "./components/Login"
+import { ProfileFeed } from "./components/ProfileFeed"
+import ExampleProfile from "./components/ExampleProfile"
 
 function App() {
   const [theme, setTheme] = useState("light")
@@ -16,6 +18,8 @@ function App() {
         <Routes>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/profilefeed" element={<ProfileFeed />} />
+          <Route path="/profileview" element={<ExampleProfile />} />
         </Routes>
       </BrowserRouter>
     </div>
