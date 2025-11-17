7a15fecc22c789eaa63a32dc57b7e7afcf4732d6
JulianAVG64 <julvargasgu@unal.edu.co>
2025-11-16 22:41:19 -0500

    Correción userservice y docker-compose

--- userservice/app/models.py
+++ userservice/app/models.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, func, Text, Index
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text, Index
+from datetime import datetime, timezone
 from .database import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    user_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String(100), nullable=False, unique=True, index=True)
     email = Column(String(254), unique=True, index=True, nullable=False)
     password_hash = Column(String, nullable=False)
@@ -12,8 +13,8 @@ class User(Base):
     last_name = Column(String(100), nullable=True)
     profile_picture_url = Column(String, nullable=False, default="")
     bio = Column(Text, nullable=False, default="")
-    created_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
+    created_at = Column(DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
     is_active = Column(Boolean, default=True, nullable=False)
     
     __table_args__ = (
